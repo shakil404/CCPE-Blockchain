843f1f9e8a2ad5fdb79a954dbd81afd54d6c3060
Giorgi <giorgi.javrishvili@gmail.com>
2017-01-15 22:09:56 +0800

    Final version of commands for webservice. start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,12 @@
 #docker rmi -f $(docker images -q)
 
 
-cd app-hyperledger
-. setenv.sh
-docker-compose -f single-peer-ca.yaml up -d
+#cd app-hyperledger
+#. setenv.sh
+#docker-compose -f single-peer-ca.yaml up -d
 
-cd ../app-webservice
-docker build -t ccpe/web-serv
+#cd ../app-webservice
+cd app-webservice
+docker build -t ccpe/ws .
 
-docker run -p 9999:3000 ccpe/web-serv
+docker run -p 9999:3000 -d ccpe/ws
