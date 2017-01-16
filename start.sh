c667cc333aa53e61777767556702d842c5a38755
Giorgi <giorgi.javrishvili@gmail.com>
2017-01-17 02:55:54 +0800

    Setting back to defaults

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 
 #!/usr/bin/env bash
 
-docker stop $(docker ps -a -q)
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -q)
+#docker stop $(docker ps -a -q)
+#docker rm -f $(docker ps -a -q)
+#docker rmi -f $(docker images -q)
 
 
 # cd app-hyperledger
@@ -15,4 +15,3 @@ cd app-webservice
 docker build -t ccpe/ws .
 
 docker run -p 9999:3000 -d ccpe/ws
-#docker run --name ccpe_node --net=bridge -p 9999:3000 ccpe/ws
