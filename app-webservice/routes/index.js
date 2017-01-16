289ae1a19f8f698e61d53c2c80887edfe982c03c
Giorgi <giorgi.javrishvili@gmail.com>
2017-01-17 01:24:20 +0800

    Run Blockchain network with single peer

--- app-webservice/routes/index.js
+++ app-webservice/routes/index.js
@@ -2,6 +2,64 @@ var express = require('express');
 var router = express.Router();
 
 
+ // Step 1 ================================== 
+    var Ibc1 = require('ibm-blockchain-js');
+    var ibc = new Ibc1(/*logger*/);             //you can pass a logger such as winston here - optional 
+    var chaincode = {};
+ 
+    // ================================== 
+    // configure ibc-js sdk 
+    // ================================== 
+    var options =   {
+        network:{
+            peers:   [{
+                "api_host": "172.17.0.2",
+                "api_port": 7050,
+                //"api_port_tls": xxx,
+                //"id": "xxxxxx-xxxx-xxx-xxx-xxxxxxxxxxxx_vpx"
+                "id": "app_peer"
+            }],
+            users:  [{
+                "enrollId": "user1",
+                "enrollSecret": "MS9qrN8hFjlE"
+            }],
+            options: {              //this is optional 
+                quiet: true, 
+                timeout: 60000,
+                tls: false,
+            }
+        },
+        chaincode:{
+            zip_url: 'https://github.com/ibm-blockchain/marbles-chaincode/archive/master.zip',
+            unzip_dir: 'marbles-chaincode-master/part2_v1.0.0',
+            git_url: 'https://github.com/ibm-blockchain/marbles-chaincode/part2_v1.0.0'
+        }
+    };
+    
+    // Step 2 ================================== 
+    ibc.load(options, cb_ready);
+ 
+    // Step 3 ================================== 
+    function cb_ready(err, cc){               //response has chaincode functions 
+        app1.setup(ibc, cc);
+        app2.setup(ibc, cc);
+    
+    // Step 4 ================================== 
+        if(cc.details.deployed_name === ""){        //decide if I need to deploy or not 
+            cc.deploy('init', ['99'], null, cb_deployed);
+        }
+        else{
+            console.log('chaincode summary file indicates chaincode has been previously deployed');
+            cb_deployed();
+        }
+    }
+ 
+    // Step 5 ================================== 
+    function cb_deployed(err){
+        console.log('sdk has deployed code and waited');
+        chaincode.query.read(['a']);
+    }
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +67,7 @@ router.get('/', function(req, res, next) {
 });
 
 
+/* Example of GET method, simple respond with a string */
 router.get('/transaction', function(req, res) {
   var responseObject =  { message: "Get Transaction Page" };
   res.send(responseObject);
@@ -16,6 +75,7 @@ router.get('/transaction', function(req, res) {
 
 
 
+/* Get POST data coming from Exchange APP */
 router.post('/getTransaction', function(req, res, next) {
 	console.log('savedata called');
     console.log(req.body);
@@ -26,7 +86,7 @@ router.post('/getTransaction', function(req, res, next) {
     var sellerB = req.body.Seller_B;    
     var pointA = req.body.Ex_points_A;
     var pointB = req.body.Ex_points_B;
-    console.log(req.body.User_A);
+    //console.log(req.body.User_A);
 
 	var responseObject =  { "message": "Transaction accepted",
 							"respond": true };
--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 
 #!/usr/bin/env bash
 
-#docker stop $(docker ps -a -q)
-#docker rm -f $(docker ps -a -q)
-#docker rmi -f $(docker images -q)
+docker stop $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -q)
 
 
-#cd app-hyperledger
-#. setenv.sh
-#docker-compose -f single-peer-ca.yaml up -d
+cd app-hyperledger
+. setenv.sh
+docker-compose -f single-peer-ca.yaml up -d
 
-#cd ../app-webservice
-cd app-webservice
+cd ../app-webservice
+#cd app-webservice
 docker build -t ccpe/ws .
 
 docker run -p 9999:3000 -d ccpe/ws
