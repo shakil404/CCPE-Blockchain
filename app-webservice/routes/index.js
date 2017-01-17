5e03cd0348443cb1a57b643e9b879d5908bb2b08
Giorgi <giorgi.javrishvili@gmail.com>
2017-01-18 03:04:18 +0800

    Update on blockchain

--- app-webservice/routes/index.js
+++ app-webservice/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var g_cc;
 
  // Step 1 ================================== 
     var Ibc1 = require('ibm-blockchain-js');
@@ -39,29 +40,36 @@ var router = express.Router();
     // Step 2 ================================== 
     ibc.load(options, cb_ready);
  
-    // Step 3 ================================== 
-    function cb_ready(err, cc){               //response has chaincode functions 
+    // Step 3 ==================================
+    function cb_ready(err, cc){                             //response has chaincode functions
         //app1.setup(ibc, cc);
         //app2.setup(ibc, cc);
-    
-    // Step 4 ================================== 
-        if(cc.details.deployed_name === ""){        //decide if I need to deploy or not 
-            cc.deploy('init', ['99'], null, cb_deployed);
+
+    // Step 4 ==================================
+        if(true){                //decide if I need to deploy or not
+            g_cc = cc;
+            cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){                       //delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
+                console.log("success deployed");
+                cb_deployed();
+            });
         }
         else{
+            g_cc = cc;
             console.log('chaincode summary file indicates chaincode has been previously deployed');
+         
             cb_deployed();
         }
     }
- 
-    // Step 5 ================================== 
+
+    // Step 5 ==================================
     function cb_deployed(err){
         console.log('sdk has deployed code and waited');
         //chaincode.query.read(['a']);
         http.listen(3000, function(){
-            console.log('listening on *:3000');
+          console.log('listening on *:3000');
+          
         });
-    }
+}
 
 
 /* GET home page. */
